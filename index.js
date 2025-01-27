ca7088bc01a106ef8925daee0c99d8143b92fbaf
Sonu Sahu <70482741+SONUSAHUNAYAGAON@users.noreply.github.com>
2025-01-27 22:07:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mysql = require("mysql2");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 // MySQL Connection
 const db = mysql.createConnection({
